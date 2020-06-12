4b86f01b15321c8a802d9c0ffaec4cc36be4de7d
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2020-06-12 15:56:01 +0200

    🚧 dinglehopper: Use a Bootstrap tooltip for the segment id

--- qurator/dinglehopper/templates/report.html.js
+++ qurator/dinglehopper/templates/report.html.js
@@ -1,19 +1,15 @@
 function find_diff_class(classes) {
-    return classes.split(/\s+/).find(x => x.match(/.diff\d.*/));
+    return $('.' + classes.split(/\s+/).find(x => x.match(/.diff\d.*/)));
 }
 
 $(document).ready(function() {
-    $('.diff').mouseover(function() {
-        let c = find_diff_class($(this).attr('class'));
-        $('.' + c).addClass('diff-highlight');
+    /* Enable Bootstrap tooltips */
+    $('[data-toggle="tooltip"]').tooltip();
 
-        segment_id = $(this).attr('data-segment-id');
-        $(this).closest('div').find('.status-box').text(segment_id);
+    $('.diff').mouseover(function() {
+        find_diff_class($(this).attr('class')).addClass('diff-highlight');
     });
     $('.diff').mouseout(function() {
-        let c = find_diff_class($(this).attr('class'));
-        $('.' + c).removeClass('diff-highlight');
-
-        $(this).closest('div').find('.status-box').text('');
+        find_diff_class($(this).attr('class')).removeClass('diff-highlight');
     });
 });
