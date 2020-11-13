c9219cbacd58c858191944a0811d82f947cea0bf
Benjamin Rosemann <benjamin.rosemann@la-bw.de>
2020-11-13 09:01:33 +0100

    Make sure that 0 cer and wer are reported

--- qurator/dinglehopper/tests/test_integ_cli_valid_json.py
+++ qurator/dinglehopper/tests/test_integ_cli_valid_json.py
@@ -58,3 +58,18 @@ def test_cli_json_cer_is_infinity(tmp_path):
             j = json.load(jsonf)
             assert j["cer"] == pytest.approx(float("inf"))
             assert j["fca"] == pytest.approx(-13)
+
+
+def test_cli_json_cer_0_in_report(tmp_path):
+    """Test that the cli/process() yields a loadable JSON report when CER == 0"""
+
+    with working_directory(str(tmp_path)):
+        with open("gt.txt", "w") as gtf:
+            gtf.write("Lorem Ipsum")
+
+        process("gt.txt", "gt.txt", "report", metrics="cer,wer,fca")
+        with open("report.json", "r") as jsonf:
+            j = json.load(jsonf)
+            assert j["cer"] == pytest.approx(0)
+            assert j["wer"] == pytest.approx(0)
+            assert j["fca"] == pytest.approx(1)
