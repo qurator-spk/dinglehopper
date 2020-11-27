a68fc269d9941247bfd32ba0c1f8445194124654
Benjamin Rosemann <benjamin.rosemann@la-bw.de>
2020-11-27 11:18:11 +0100

    Fix the extraction of text from Page with TableRegion
    Dinglehopper did not consider `OrderedGroupIndex` in the `ReadingOrder`
    element when extracting text regions. As a consequence a `TableRegion`
    was not considered for text extraction.

---
+++ qurator/dinglehopper/tests/test_integ_table_extraction.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+from lxml import etree as ET
+
+from .. import page_text
+
+
+@pytest.mark.parametrize(
+    "file,expected_text",
+    [
+        ("table-order-0001.xml", "1\n2\n3\n4\n5\n6\n7\n8\n9"),
+        ("table-order-0002.xml", "1\n4\n7\n2\n5\n8\n3\n6\n9"),
+        ("table-region.xml", "1\n2\n3\n4\n5\n6\n7\n8\n9"),
+        ("table-no-reading-order.xml", "5\n6\n7\n8\n9\n1\n2\n3\n4"),
+        ("table-unordered.xml", "5\n6\n7\n8\n9\n1\n2\n3\n4"),
+    ],
+)
+@pytest.mark.integration
+def test_reading_order_settings(file, expected_text):
+    data_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "data", "table-order"
+    )
+    if "table-unordered.xml" == file:
+        with pytest.raises(NotImplementedError):
+            page_text(ET.parse(os.path.join(data_dir, file)))
+    else:
+        ocr = page_text(ET.parse(os.path.join(data_dir, file)))
+        assert ocr == expected_text
