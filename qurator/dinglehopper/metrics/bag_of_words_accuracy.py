9f5112f8f674069c1795fba9dc441e7c2786c484
Benjamin Rosemann <benjamin.rosemann@la-bw.de>
2021-06-11 10:23:26 +0200

    Remove support for ExtractedText for bag metrics.

--- qurator/dinglehopper/metrics/bag_of_words_accuracy.py
+++ qurator/dinglehopper/metrics/bag_of_words_accuracy.py
@@ -1,20 +1,12 @@
 from collections import Counter
-from typing import Union
 
 from .utils import bag_accuracy, MetricResult, Weights
-from .. import ExtractedText
 from ..normalize import words_normalized
 
 
 def bag_of_words_accuracy(
-    reference: Union[str, ExtractedText],
-    compared: Union[str, ExtractedText],
-    weights: Weights,
+    reference: str, compared: str, weights: Weights
 ) -> MetricResult:
-    if isinstance(reference, ExtractedText):
-        reference = reference.text
-    if isinstance(compared, ExtractedText):
-        compared = compared.text
     reference_words = Counter(words_normalized(reference))
     compared_words = Counter(words_normalized(compared))
     result = bag_accuracy(reference_words, compared_words, weights)
