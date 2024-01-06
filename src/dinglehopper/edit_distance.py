90ecb132e10d9e1012a759d6963c4f93512fbbb1
Sadra Barikbin <sadraqazvin1@yahoo.com>
2024-01-07 00:08:18 +0330

    Fix a typo

--- src/dinglehopper/edit_distance.py
+++ src/dinglehopper/edit_distance.py
@@ -3,7 +3,6 @@ from typing import List
 
 from multimethod import multimethod
 from rapidfuzz.distance import Levenshtein
-from typing import List
 from uniseg.graphemecluster import grapheme_clusters
 
 from .extracted_text import ExtractedText
